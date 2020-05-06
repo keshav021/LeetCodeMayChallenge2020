5ff341d7800e2cdd6fac5f593703a6df1907d742
Keshav Agarwal <keshavag007@gmail.com>
2020-05-06 13:10:12 +0530

    Update MajorityNumber.java

--- MajorityNumber.java
+++ MajorityNumber.java
@@ -2,7 +2,7 @@ class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<n;i++){
             if( hm.containsKey(nums[i]) ){
                 hm.put(nums[i], hm.get(nums[i])+1 );
             }
